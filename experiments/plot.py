bdc3f191f9755e2028e1b2eeb4b732b2d97aae87
Joel Sandman <joel@sandman.nu>
2021-05-03 17:09:45 +0200

    Changed plot file type from PNG to SVG, removed titles for plots, added subdirectory for plot files

--- experiments/plot.py
+++ experiments/plot.py
@@ -39,7 +39,7 @@ def plot_network_traffic_reduction_by_requests():
     plt.ylabel('Network Traffic Reduction')
     plt.legend()
     plt.show()
-    # plt.savefig('network_traffic_reduction_by_requests.png', bbox_inches='tight')
+    # plt.savefig('plots/network_traffic_reduction_by_requests.svg', bbox_inches='tight')
 
 def plot_network_traffic_reduction_by_size():
     ttls = [0.1, 0.2, 0.5, 1, 2, 5, 10, 20]
@@ -51,12 +51,11 @@ def plot_network_traffic_reduction_by_size():
     plt.plot(ttls, [0]*len(ttls), 'g-', label='No caching')
     plt.xscale('log')
     plt.xticks(ttls, ttls)
-    plt.suptitle('Network Traffic Reduction by Size')
     plt.xlabel('TTL (s)')
     plt.ylabel('Network Traffic Reduction')
     plt.legend()
     plt.show()
-    # plt.savefig('plot_network_traffic_reduction_by_size.png', bbox_inches='tight')
+    # plt.savefig('plots/plot_network_traffic_reduction_by_size.svg', bbox_inches='tight')
 
 def plot_data_staleness_by_requests():
     ttls = [0.1, 0.2, 0.5, 1, 2, 5, 10, 20]
@@ -68,12 +67,11 @@ def plot_data_staleness_by_requests():
     plt.plot(ttls, [0]*len(ttls), 'g-', label='No caching')
     plt.xscale('log')
     plt.xticks(ttls, ttls)
-    plt.suptitle('Data Staleness by Requests')
     plt.xlabel('TTL (s)')
     plt.ylabel('Data Staleness')
     plt.legend()
     plt.show()
-    # plt.savefig('plot_data_staleness_by_requests.png', bbox_inches='tight')
+    # plt.savefig('plots/plot_data_staleness_by_requests.svg', bbox_inches='tight')
 
 def plot_data_staleness_by_size():
     ttls = [0.1, 0.2, 0.5, 1, 2, 5, 10, 20]
@@ -85,12 +83,11 @@ def plot_data_staleness_by_size():
     plt.plot(ttls, [0]*len(ttls), 'g-', label='No caching')
     plt.xscale('log')
     plt.xticks(ttls, ttls)
-    plt.suptitle('Data Staleness by Size')
     plt.xlabel('TTL (s)')
     plt.ylabel('Data Staleness')
     plt.legend()
     plt.show()
-    # plt.savefig('plot_data_staleness_by_size.png', bbox_inches='tight')
+    # plt.savefig('plots/plot_data_staleness_by_size.svg', bbox_inches='tight')
 
 def plot_memory_usage():
     ttls = [0.1, 0.2, 0.5, 1, 2, 5, 10, 20]
@@ -102,12 +99,11 @@ def plot_memory_usage():
     plt.plot(ttls, [0]*len(ttls), 'g-', label='No caching')
     plt.xscale('log')
     plt.xticks(ttls, ttls)
-    plt.suptitle('Memory Usage')
     plt.xlabel('TTL (s)')
     plt.ylabel('Memory Usage (bytes)')
     plt.legend()
     plt.show()
-    # plt.savefig('plot_memory_usage.png', bbox_inches='tight')
+    # plt.savefig('plots/plot_memory_usage.svg', bbox_inches='tight')
 
 def plot_network_traffic_reduction_divided_by_data_staleness():
     ttls = [0.1, 0.2, 0.5, 1, 2, 5, 10, 20]
@@ -125,12 +121,11 @@ def plot_network_traffic_reduction_divided_by_data_staleness():
     # plt.plot(ttls, [1]*len(ttls), 'g-', label='No caching')
     plt.xscale('log')
     plt.xticks(ttls, ttls)
-    plt.suptitle('Network Traffic Reduction / Date Staleness')
     plt.xlabel('TTL (s)')
     plt.ylabel('Network Traffic Reduction / Date Staleness')
     plt.legend()
     plt.show()
-    # plt.savefig('plot_memory_usage.png', bbox_inches='tight')
+    # plt.savefig('plots/plot_memory_usage.svg', bbox_inches='tight')
 
 def plot_network_traffic_reduction_for_ttl():
     methodology = ['fine-grained']
@@ -141,13 +136,11 @@ def plot_network_traffic_reduction_for_ttl():
     plt.bar(1, full_page_network_traffic_reduction[4], color='red', label='Full-page caching')
     plt.bar(2, 0, color='green', label='No caching')
     plt.xticks([0, 1, 2], ['Fine-grained', 'Full-page', 'No cache'])
-    plt.suptitle('Network Traffic Reduction for TTL = 2 seconds')
     plt.xlabel('Methodology')
     plt.ylabel('Network Traffic Reduction')
     plt.legend()
     plt.show()
-    # plt.savefig('network_traffic_reduction_by_requests.png', bbox_inches='tight')
-
+    # plt.savefig('plots/network_traffic_reduction_by_requests.svg', bbox_inches='tight')
 
 if __name__=="__main__":
     # plot_network_traffic_reduction_by_requests()
